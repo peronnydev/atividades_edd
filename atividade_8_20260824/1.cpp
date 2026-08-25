b1e2ed0d0c06b4c669772d2e12927eca437f48b4
peronnydev <peronnydev@gmail.com>
2026-08-24 21:29:38 -0300

    Atividade 8

---
+++ atividade_8_20260824/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int vetor[10] = {10,9,8,7,6,5,4,3,2,1};
+int tamanhoDoVetor = 10;
+int contador = 0;
+
+void ordenarLista(int vetor[]){
+	while(contador < tamanhoDoVetor){
+		for(int i = 0; i < (tamanhoDoVetor - 1); i++){
+			int numeroAtual = vetor[i];
+			contador ++;
+			if(numeroAtual > vetor[i + 1]){
+				contador = 0;
+				vetor[i] = vetor[i + 1];
+				vetor[i + 1] = numeroAtual;
+			} 
+		}
+	}
+}
+
+int main(){
+	ordenarLista(vetor);
+	for(int i = 0; i < tamanhoDoVetor; i++){
+		cout << vetor[i];
+	}
+}
